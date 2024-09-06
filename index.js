2552d9aab9ad05acf0084b7fbd3695482a79e598
Daria Oznobishina <hyungwonaaaaaa@gmail.com>
2024-09-06 12:42:51 +0300

    thirdCommit

--- index.js
+++ index.js
@@ -1,88 +1,42 @@
-//TODO Создайте кнопку на странице. При нажатии на кнопку должен выводиться текст "Hello, World!".
-let buttonHello = document.getElementById("buttonHello");
-let helloText = document.getElementById("helloText");
+const form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
+  const name = document.getElementById("name").value;
+  const login = document.getElementById("login").value;
+  const age = document.getElementById("age").value;
+  let errorMessages = [];
 
-buttonHello.addEventListener("click", () => {
-  helloText.textContent = helloText.textContent == "" ? "Hello World" : "";
-});
+  !validateName(name)
+    ? errorMessages.push(
+        "Имя пользователя должно содержать от 1 до 50 символов"
+      )
+    : null;
 
-//TODO Создайте поле ввода на странице. При нажатии на клавишу "Enter" должен выводиться текст, введенный в поле ввода.
-let inputTextEnter = document.getElementById("inputTextEnter");
-let textEnter = document.getElementById("textEnter");
+  !validateName(age)
+    ? errorMessages.push("Возраст содержит только цифры и от 1 до 3 цифр.")
+    : null;
 
-inputTextEnter.addEventListener("keydown", (event) => {
-  textEnter.textContent =
-    event.key == "Enter" ? inputTextEnter.value : textEnter.textContent;
-});
-//TODO Создайте две кнопки на странице. При нажатии на первую кнопку должен выводиться текст "Нажата первая кнопка", а при нажатии на вторую кнопку должен выводиться текст "Нажата вторая кнопка".
-let textButtons = document.getElementById("textButtons");
-let buttonTextOne = document.getElementById("buttonTextOne");
-let buttonTextTwo = document.getElementById("buttonTextTwo");
+  !validateLogin(login)
+    ? errorMessages.push(
+        "Логин пользователя содержит от 4 до 20 символов и включает только буквы и цифры."
+      )
+    : null;
 
-buttonTextOne.addEventListener("click", () => {
-  textButtons.textContent = "Нажата первая кнопка";
-});
-
-buttonTextTwo.addEventListener("click", () => {
-  textButtons.textContent = "Нажата вторая кнопка";
-});
-//TODO Создайте блок на странице. При наведении мыши на блок должен меняться его цвет на красный.
-let backColor = document.getElementById("backColor");
-backColor.addEventListener("mousemove", () => {
-  backColor.style.backgroundColor = "red";
-});
-backColor.addEventListener("mouseout", () => {
-  backColor.style.backgroundColor = "brown";
-});
-//TODO Создайте две кнопки на странице. При нажатии на первую кнопку должны появиться подробности, а при нажатии на вторую кнопку должны скрыться подробности.
-let descript = document.getElementById("descript");
-let buttonDescOne = document.getElementById("buttonDescOne");
-let buttonDescTwo = document.getElementById("buttonDescTwo");
-
-buttonDescOne.addEventListener("click", () => {
-  descript.textContent = "Подробности";
+  if (errorMessages.length > 0) {
+    alert(errorMessages.join("\n"));
+    event.preventDefault();
+  }
 });
 
-buttonDescTwo.addEventListener("click", () => {
-  descript.textContent = "";
-});
-//TODO Создайте поле ввода на странице. При фокусировке на поле ввода должен меняться его цвет на желтый.
-let inputFocus = document.getElementById("inputFocus");
-inputFocus.addEventListener("focusin", () => {
-  inputFocus.style.backgroundColor = "yellow";
-});
-inputFocus.addEventListener("focusout", () => {
-  inputFocus.style.backgroundColor = "white";
-});
-//TODO Создайте кнопку на странице. При двойном клике на кнопку должен выводиться текст "Кнопка была дважды нажата".
-let inputDoubleClick = document.getElementById("inputDoubleClick");
-let buttonDoubleClick = document.getElementById("buttonDoubleClick");
+function validateLogin(login) {
+  const checkLogin = /^[a-zA-Z0-9]{4,20}$/;
+  return checkLogin.test(login);
+}
 
-buttonDoubleClick.addEventListener("dblclick", () => {
-  inputDoubleClick.textContent = "Кнопка была дважды нажата";
-});
+function validateAge(age) {
+  let checkAge = /^[0-9]{1,3}$/;
+  return checkAge.test(age);
+}
 
-//TODO cоздайте ссылку на странице. При наведении мыши на ссылку должен меняться ее цвет на зеленый.
-let linkColor = document.getElementById("linkColor");
-linkColor.addEventListener("mousemove", () => {
-  linkColor.style.color = "green";
-});
-linkColor.addEventListener("mouseout", () => {
-  linkColor.style.color = "blue";
-});
-
-//TODO Создайте две кнопки на странице. При нажатии на одну кнопку должен увеличиваться ее размер на вторую уменьшаться.
-let buttonAnimTwo = document.getElementById("buttonAnimTwo");
-let buttonAnimOne = document.getElementById("buttonAnimOne");
-
-let btn_scale1 = 1;
-let btn_scale2 = 1;
-
-buttonAnimOne.addEventListener("click", () => {
-  btn_scale1+=0.2;
-  buttonAnimOne.style.scale = btn_scale1;
-});
-buttonAnimTwo.addEventListener("click", () => {
-  btn_scale2-=0.2;
-  buttonAnimTwo.style.scale = btn_scale2;
-});
+function validateName(name) {
+  return name.length >= 1 && name.length <= 50;
+}
--- lessonJS.js
+++ lessonJS.js
@@ -1,4 +1,4 @@
-let div = document.getElementById("square");
+// let div = document.getElementById("square");
 // let result = document.getElementById("result");
 // div.addEventListener("dblclick", () => {
 //   alert("div нажат");
@@ -60,25 +60,72 @@ let div = document.getElementById("square");
 //   }
 // });
 
-document.addEventListener("keydown", (event) => {
-  let position = div.getBoundingClientRect();
-  let step = 10;
+// document.addEventListener("keydown", (event) => {
+//   let position = div.getBoundingClientRect();
+//   let step = 10;
 
-  div.style.position = "absolute";
-  switch (event.code) {
-    case "ArrowUp":
-      div.style.top = position.top - step + "px";
-      break;
-    case "ArrowDown":
-      div.style.top = position.top + step + "px";
-      break;
-    case "ArrowLeft":
-      div.style.left = position.left - step + "px";
-      break;
-    case "ArrowRight":
-      div.style.left = position.left + step + "px";
-      break;
-  }
-});
+//   div.style.position = "absolute";
+//   switch (event.code) {
+//     case "ArrowUp":
+//       div.style.top = position.top - step + "px";
+//       break;
+//     case "ArrowDown":
+//       div.style.top = position.top + step + "px";
+//       break;
+//     case "ArrowLeft":
+//       div.style.left = position.left - step + "px";
+//       break;
+//     case "ArrowRight":
+//       div.style.left = position.left + step + "px";
+//       break;
+//   }
+// });
+
+//TODO 3 LESSON
+
+// let regex = /./; выбирает любой символ, кроме символа новой строки
+// let regex = /^Hello/; поиск по началу строки 
+// let regex = /world!$/; поиск конца строки
+// let regex = /\d/; поиск цифры
+// let regex = /\D/; поиск любого символа кроме цифр
+// let regex = /\w/; поиск по цифрам, буквам или подчеркивание
+// let regex = /\W/; поиск по символам, кроме цифр, букв или подчеркиваний
+// let regex = /a*/ = * повторение 0 или более
+// let regex = /a+/;
+
+// let regex = /a?/ = ? повторение 0 или 1
+// let regex = /a{3}/; проверяет количетсов повторений
+// let regex = /a{2,4}/; {n,n} проверяет заданный промежуток
+// let regex = /(abc)/; ищет совпадения, в любом месте строки
+// let regex = /a|b/; ищет один из вариантов, в любом месте строки
+// let regex = /[abc]/; ищет один из множества вариантов
+// let regex = /[^abc]/ ищет любой символ кроме указанных
+// let regex = /[a-zA-Z]/ поиск по символам в диапозоне 
+// let regex = /[A-z]/ 
+// let regex = /^\d+$/ проверка на число
+// let regex = /^[a-zA-Z0-9]+$/
+// let regex = /^[A-z0-9]+$/
+// let regex = /^[!"#%:,.;((]+$/
+
+// let regex = /\d+/
+
+// console.log(regex.test("t32434rte:"));
 
 
+// console.log('1'.match(regex));
+// console.log('fffrce'.match(regex));
+
+
+// let str = 'abc123'
+
+// console.log(str.match(/\d+/));
+
+
+// let str = 'abc123'
+
+// console.log(str.replace(/\d+/, ''));
+
+
+let str = 'abc123'
+
+console.log(str.replace(/\d+/, ''));
